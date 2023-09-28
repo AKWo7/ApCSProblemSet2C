a4b0ae307ea54ac92306d9a922b23ac563b915f7
AKWo7 <110649642+AKWo7@users.noreply.github.com>
2023-09-28 11:01:23 -0700

    Add files via upload

---
+++ OrderedPair.java
@@ -0,0 +1,26 @@
+public class OrderedPair
+{
+   double X, Y;
+   
+   public OrderedPair(){
+       X = 0;
+       Y = 0;
+   }
+   
+   public OrderedPair(double x, double y){
+       X = x;
+       Y = y;
+   }
+   
+   public String toString(){
+       return X+", "+Y;
+   }
+   
+   public double getX(){
+       return X;
+   }
+   
+   public double getY(){
+       return Y;
+   }
+}
